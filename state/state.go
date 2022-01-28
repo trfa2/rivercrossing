f134a22afa8a7e53ec2fd95e37354acb1e5317ce
trfa2 <trymfalkum@gmail.com>
2022-01-28 14:14:10 +0100

    Started on package state

---
+++ state/state.go
@@ -0,0 +1,17 @@
+package state
+
+import "fmt"
+
+var (
+	name string
+)
+
+func ViewState() string (
+	return "[kylling rev korn mann ---\ \__/ _________________/---]"
+
+
+func main() {
+
+	fmt.Println(ViewState());
+
+}
